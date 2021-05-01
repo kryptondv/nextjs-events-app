3e79987096624476b3360bf93157b6cbdff37d1d
kryptondv <kryptondevpl@gmail.com>
2021-05-01 09:04:04 +0200

    added head to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Club Events</title>
+        <meta name="description" content="Welcome to Club Events" />
+      </Head>
       <h1>Home</h1>
     </div>
-  )
+  );
 }
