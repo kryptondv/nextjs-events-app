6dfe9c5ca17477f9f882103bd9a1dd0627284556
kryptondv <kryptondevpl@gmail.com>
2021-05-01 10:47:23 +0200

    added showcase component & images

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import Layout from './layout/layout';
 import Header from './header/header';
 import Footer from './footer/footer';
+import Showcase from './showcase/showcase';
 
-export { Layout, Header, Footer };
+export { Layout, Header, Footer, Showcase };
---
+++ components/showcase/showcase.js
@@ -0,0 +1,10 @@
+import styles from './showcase.module.scss';
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h2>Your electronic music party guide</h2>
+      <p>Find the best events in your area</p>
+    </div>
+  )
+}
